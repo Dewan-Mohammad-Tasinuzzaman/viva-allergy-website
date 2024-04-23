22f939cedb0191903e0d37396728dff1919c53a1
Dewan Mohammad Tasinuzzaman <dewanmohammad.tasinuzzaman@ontariotechu.net>
2024-04-23 05:14:04 -0400

    Small fixes that hopefully won't break nothing

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
-import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default withNextVideo(nextConfig);
+export default nextConfig;
